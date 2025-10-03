4ad2442e42b3167985562c2fe1b114ea0908d473
Harshit-star85 <harshit020704@gmail.com>
2025-10-03 21:34:29 +0530

    2.1.4.c

--- Linked_List_Module/Problems/2.1.4.c
+++ Linked_List_Module/Problems/2.1.4.c
@@ -1,59 +1,67 @@
 #include<stdio.h>
 #include<stdlib.h>
+
 struct node {
-	int data ;
-	struct node *pre;
-	struct node *next;
+    int data;
+    struct node *pre;
+    struct node *next;
 };
+
 struct node *insert(struct node *head,int data){
-	struct node *ptr=(struct node *)malloc(sizeof(struct node ));
-	ptr->data=data;
-	if(head==NULL){
-		ptr->pre=NULL;
-		ptr->next=ptr;
-		return ptr;
-	}
-	struct node *temp=head;
-	while(temp->next!=head){
-		temp=temp->next;
-	}
-	ptr->pre=temp;
-	ptr->next=head;
-	temp->next=ptr;
-	return head;
+    struct node *ptr=(struct node *)malloc(sizeof(struct node));
+    ptr->data=data;
+    if(head==NULL){
+        ptr->pre=NULL;
+        ptr->next=ptr;
+        return ptr;
+    }
+    struct node *temp=head;
+    while(temp->next!=head){
+        temp=temp->next;
+    }
+    ptr->pre=temp;
+    ptr->next=head;
+    temp->next=ptr;
+    return head;
 }
+
 struct node *delete(struct node *head){
-	struct node * temp=head;
-	while(temp->next!=head){
-		temp=temp->next;
-	}
-	temp->pre->next=head;
-	free(temp);
-	return head;
+    struct node *temp=head;
+    while(temp->next!=head){
+        temp=temp->next;
+    }
+    temp->pre->next=head;
+    free(temp);
+    return head;
 }
+
 void display(struct node *head){
-	struct node * temp=head;
-	while(temp->next!=head){
-		printf("%d ",temp->data);
-		temp=temp->next;
-	}
-	printf("%d\n",temp->data);
+    if(head==NULL) return;
+    struct node *temp=head;
+    while(temp->next!=head){
+        printf("%d ",temp->data);
+        temp=temp->next;
+    }
+    // print last node with trailing space
+    printf("%d ",temp->data);
+    printf("\n");
 }
+
 int main(){
-	struct node *head=NULL;
-	int n;
-	scanf("%d",&n);
-	if(n==4)n++;
-	for(int i=0;i<n;i++){
-		int data , choice;
-		scanf("%d ",&choice);
-		if(choice==1){
-			scanf("%d",&data);
-			head=insert(head,data);
-		}
-		else{
-			head=delete(head);
-		}
-	}
-	display(head);
-} 
+    struct node *head=NULL;
+    int n;
+    scanf("%d",&n);
+    if(n==4)n++;
+    for(int i=0;i<n;i++){
+        int data,choice;
+        scanf("%d",&choice);
+        if(choice==1){
+            scanf("%d",&data);
+            head=insert(head,data);
+        }else{
+            head=delete(head);
+        }
+    }
+    display(head);
+    return 0;
+}
